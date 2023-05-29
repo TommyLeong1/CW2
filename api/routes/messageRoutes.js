733afd04961163621de7fbd5c4774b8246ee6416
TommyLeong1 <122337479+TommyLeong1@users.noreply.github.com>
2023-05-29 03:43:54 +0000

    add messageRoutes.js

---
+++ api/routes/messageRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const messageController = require('../controllers/messageController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.post('/', authMiddleware, messageController.createMessage);
+router.get;
